953a9e89f0afe0f292302f9f88b41f8d7d31616f
Mayank.Rai <mayank.rai@emorphis.in>
2026-06-01 16:37:47 +0530

    Enable self-hosted

---
+++ src/main/java/com/dbHere/dbHere/testController/test.java
@@ -0,0 +1,13 @@
+package com.dbHere.dbHere.testController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @GetMapping("/hello")
+        public String test(){
+            return"welcome to dbHere";
+        }
+    }
+
