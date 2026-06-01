af724f5b9c11115d17eb7edc25bbc585ba48fb48
Mayank.Rai <mayank.rai@emorphis.in>
2026-06-01 14:57:00 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/dbHere/dbHere/DbHereApplication.java
@@ -0,0 +1,13 @@
+package com.dbHere.dbHere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbHereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbHere/dbHere/DbHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbHere.dbHere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
